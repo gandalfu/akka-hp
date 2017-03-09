9d732e9c27df14be04c4d13af09a0137e5d19918
Fidel Perez <fiperez@ncl.com>
2017-03-09 10:35:16 -0500

    Fixing dump filename

--- src/main/scala/hp.scala
+++ src/main/scala/hp.scala
@@ -17,36 +17,36 @@ class HoneyPotServer extends Actor {
     override def preStart {
       import context.system
       val manager = IO(Tcp)
-      manager ! Tcp.Bind (self, new InetSocketAddress(4443))
-      //manager ! Tcp.Bind (self, new InetSocketAddress(80))
+      manager ! Tcp.Bind (self, new InetSocketAddress(443))
+      manager ! Tcp.Bind (self, new InetSocketAddress(80))
 
   }
 
   def receive = {
       case Tcp.Connected(localAddress, remoteAddress) => {
           log.info(s"[New connection, local address, remote address] [${localAddress}] [${remoteAddress}]")
-          sender ! Tcp.Register(context.actorOf(HoneyPotConnectionHandler.props(remoteAddress, sender)))
+          sender ! Tcp.Register(context.actorOf(HoneyPotConnectionHandler.props(remoteAddress, localAddress, sender)))
       }
   }
 }
 
 
 object HoneyPotConnectionHandler {
-    def props(remote: InetSocketAddress, connection: ActorRef): Props =
-        Props(new HoneyPotConnectionHandler(remote, connection))
+    def props(remote: InetSocketAddress, local: InetSocketAddress, connection: ActorRef): Props =
+        Props(new HoneyPotConnectionHandler(remote, local, connection))
 }
 
-class HoneyPotConnectionHandler(remote: InetSocketAddress, connection: ActorRef) extends Actor {
+class HoneyPotConnectionHandler(remote: InetSocketAddress, local: InetSocketAddress, connection: ActorRef) extends Actor {
     val log = Logging(context.system, this)
 
     val now = System.currentTimeMillis / 1000
-    val dumpFileName = s"$now-${normamlize(remote.toString)}.dat"
+    val dumpFileName = s"$now-${normalize(local.toString)}-${normalize(remote.toString)}.dat"
 
     log.debug(s"Dumping to $dumpFileName")
 
     context.watch(connection)
 
-    def normamlize(aStr:String):String = aStr.replaceAll("\\.", "_").replaceAll("/", "").replaceAll(":", "-")
+    def normalize(aStr:String):String = aStr.replaceAll("\\.", "_").replaceAll("/", "").replaceAll(":", "-")
 
     def dumpData(data:ByteString) = {
         val fw = new FileWriter(dumpFileName, true)
